cb0b1fb91b01ba7685ecda62742cf65d73467fcf
окси <83812355+oxijoined@users.noreply.github.com>
2023-01-15 19:03:10 +0300

    Second

--- main.py
+++ main.py
@@ -6,18 +6,22 @@ import logging
 
 logging.basicConfig(
     level=logging.DEBUG,
-    filename = "mylog.log",
+    filename="mylog.log",
     encoding='utf-8',
-    format = "%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
+    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
     datefmt='%H:%M:%S',
-    )
+)
 
 load_dotenv()
 
 telegramToken = os.getenv("TelegramToken")
 
-bot = telebot.TeleBot(telegramToken, parse_mode='HTML',threaded=True)
+bot = telebot.TeleBot(telegramToken, parse_mode='HTML', threaded=True)
 
+
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.reply_to(message, 'hi')
 
 
 bot.polling()
