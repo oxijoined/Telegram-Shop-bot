3f2061034547db7da8436a26f6e7f07eb96b6bb4
окси <83812355+oxijoined@users.noreply.github.com>
2023-01-15 18:18:35 +0300

    First

---
+++ main.py
@@ -0,0 +1,23 @@
+import telebot
+import psycopg2
+import os
+from dotenv import load_dotenv
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename = "mylog.log",
+    encoding='utf-8',
+    format = "%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
+    datefmt='%H:%M:%S',
+    )
+
+load_dotenv()
+
+telegramToken = os.getenv("TelegramToken")
+
+bot = telebot.TeleBot(telegramToken, parse_mode='HTML',threaded=True)
+
+
+
+bot.polling()
